9b8fe09cb458210734bf829e67699b102fd83bf7
Victor Moscone <victormoscone@gmail.com>
2020-10-14 21:04:19 -0700

    Feat: Added passwordGen function and a test For Loop.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var characterList = {
+let characterList = {
     letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"],
     digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     special: [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"],
 }
 
-console.log(characterList.letters.length)
-console.log(characterList.digits.length)
-console.log(characterList.special.length)
+let yourPassword = []
+
+// console.log(characterList.letters.length)
+// console.log(characterList.digits.length)
+// console.log(characterList.special.length)
+
+// console.log(Math.floor(Math.random() * 11));
+
+function passwordGen(passwordLength) {
+    for (let i = 0; i < passwordLength; i++) {
+        let rL = Math.floor(Math.random() * characterList.letters.length);
+        yourPassword.push(characterList.letters[rL]);
+    }
+}
+
+passwordGen(10)
+
+console.log(yourPassword);
