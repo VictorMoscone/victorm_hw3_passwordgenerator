7f7804666d7fc4124ccdb33a92430295711eaa29
Victor Moscone <victormoscone@gmail.com>
2020-10-15 18:30:25 -0700

    Broken: Pushing broken code to my repo for assistance from others.

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ let characterList = {
     lettersList: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"],
     digitsList: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     specialList: [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"],
+    arrayList: ["lettersList", "digitsList", "specialList"],
 }
+
+console.log(characterList.arrayList);
 
 let yourPassword = []
 
-// console.log(characterList.letters.length)
-// console.log(characterList.digits.length)
-// console.log(characterList.special.length)
-
-// console.log(Math.floor(Math.random() * 11));
+// console.log(characterList.lettersList.length)
+// console.log(characterList.digitsList.length)
+// console.log(characterList.specialList.length)
 
 function passwordGen(passwordLength) {
-    // Function runs based on the number provided by the user.
+    // Function runs based on the password length number provided by the user.
     for (let i = 0; i < passwordLength; i++) {
-        let rL = Math.floor(Math.random() * characterList.lettersList.length);
-            // randomly chooses a number between 1 and lettersList length.
-        yourPassword.push(characterList.lettersList[rL]);
+        let whichList = Math.floor(Math.random() * characterList.arrayList.length);
+        console.log(whichList);
+            // Rolls for 0 - 2 to determine which array I want to pull from.
+        let chosenArray = characterList.arrayList[whichList];
+            // Assigns the selected array to chosen array.
+        console.log(chosenArray);
+        let bigRandomNumber = Math.floor(Math.random() * characterList.chosenArray.length);
+            // randomly chooses a number between 1 and the length of the chosen array.
+            // this code is not working.
+        console.log(bigRandomNumber);
+        yourPassword.push(characterList.chosenArray[bigRandomNumber]);
             // Finds which letter corresponds to the above random number and pushes it to an empty array.
     }
 }
