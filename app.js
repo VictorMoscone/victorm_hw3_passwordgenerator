14df3cae29e10e90defe737b52a2effd03716d4e
Victor Moscone <victormoscone@gmail.com>
2020-10-16 22:12:07 -0700

    Revamp: Integrated assignment html, css, and js code into mine.

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+let generateBtn = document.querySelector("#generate");
+    // Assignment Code
+
+function writePassword() {
+    // Write password to the #password input
+    let password = stringPassword;
+    let passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
+    // Add event listener to generate button
+
+let yourPassword = []
+    // Assigning yourPassword an empty array.
+
+
 let characterList = [
         lettersList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"],
         digitsList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
         specialList = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"],
     ]     
 
-let yourPassword = []
-
 function passwordGen(passwordLength) {
     // Function runs based on the password length number provided by the user.
+    // Note to self: will need to write IF statements -> min of 8, max of 128.
     for (let i = 0; i < passwordLength; i++) {
         let whichList = Math.floor(Math.random() * characterList.length);
             // Rolls for 0 - 2 to determine which array I want to pull from.
@@ -18,8 +34,16 @@ function passwordGen(passwordLength) {
     }
 }
 
+// // Additional notes to self for homework requirements:
+//     - User chooses length.
+//     - To include lowercase?
+//     - To include uppercase?
+//     - To include numerics?
+//     - To include specials?
+//     - I'm thinking that instead of a series of prompts, I want to just have checkboxes? If possible.
+
 passwordGen(10)
 
-let stringTest = yourPassword.join("");
+let stringPassword = yourPassword.join("");
     // This combines each of the password results into one string with no seperators.
-console.log(stringTest);
+console.log(stringPassword);
