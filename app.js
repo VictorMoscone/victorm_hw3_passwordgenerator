1aa3974751ef8d62826b0e58dbe4a88d7e6847cc
Victor Moscone <victormoscone@gmail.com>
2020-10-17 18:19:56 -0700

    Feat: User can dictate if they want uppercase letters.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let generateBtn = document.querySelector("#generate");
 let passContent = document.querySelector("#password");
-    // Assignment Code
 let yourPassword = []
-    // Assigning yourPassword an empty array.
+    // Assigning yourPassword an empty array. Eventually this will contain our generated password.
 let characterList = [
-    // An array with nested arrays that can easily be called on.
+    // An array with nested arrays that will dictate what our password can and can't be.
     lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"],
     upperCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
     digitsList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
@@ -23,9 +22,15 @@ function promptTime() {
         let lwrCaseOk = confirm("Yo, want lowercase letters?");
             // This will either keep or remove all lowercase letters from the array.
             if (lwrCaseOk == false) {
-                characterList.splice(0, 1);
+                let indexNum = characterList.indexOf(lowerCaseList)
+                characterList.splice(indexNum, 1);
             }
-        // var uprCaseOk = confirm("How about some uppercase letters?");
+        let uprCaseOk = confirm("How about some uppercase letters?");
+            // This will either keep or remove all uppercase letters from the array.
+            if (uprCaseOk == false) {
+                let indexNum = characterList.indexOf(upperCaseList)
+                characterList.splice(indexNum, 1);
+            }
         // var numOk = confirm("Can we roll some numbers for ya?");
         // var specOk = confirm("Lastly, are special characters okay?")
         passwordGen(passNum);
@@ -57,7 +62,7 @@ function printPassword() {
 
 // // Additional notes to self for homework requirements:
 //     - User chooses length. [Done]
-//     - To include lowercase?
+//     - To include lowercase? [Done]
 //     - To include uppercase?
 //     - To include numerics?
 //     - To include specials?
